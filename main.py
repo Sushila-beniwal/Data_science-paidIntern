a057e8f975d11e2fda0e7d13031a04fd044c5cf5
sushila <beniwalsushila963@gmail.com>
2024-08-23 11:20:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+
+
+st.markdown("<h2 style='color: #036029;'>scrap data of available doctors from practo.com</h2>",unsafe_allow_html=True)
+st.image("41667647-medical-background-with-a-doctor.jpg",width=400,caption="scrape available doctors from practo",use_column_width=False,output_format='auto',channels='RGB')
+Location = st.text_input("enter the location")
+Specialization = st.selectbox("select the specialization you need",('dermatologist','dentist','general physician','gynecologist/obstetrician','homoeopath','ayurveda','Ear-nose-throat (ent) Specialist'))
+
+
+
+
+
+
+def no_of_doctors_available(Loc,speciality):
+    #import required libraries for web scrapping
+
+    import requests
+    from bs4 import BeautifulSoup 
+    from urllib.request import urlopen
+
+
+
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36'} 
+
+    url = "https://www.practo.com/"+Loc+"/"+speciality
+
+    
+
+    try:
+        url = requests.get(url,headers=headers).text
+
+        beautify = BeautifulSoup(url,'html.parser')
+        ans = beautify.findAll('div',{"class":"c-listing-wrapper"})[0].div.div.div.div.h1
+        if ans:
+            
+            doctors_available = ans.text
+            return doctors_available
+           
+        else:
+            return "we couldn't find doctors for you"
+        
+    except requests.exceptions.RequestException as e:
+        return f"an error occured {e}"
+
+
+
+
+
+#create button using streamlit
+Button = st.button("Scrape")
+
+
+#if doctors available returns noof available doctors
+#else print no doctor is available
+
+if Button:
+    st.write(no_of_doctors_available(Location,Specialization))
+
+
+
